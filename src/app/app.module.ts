646d7ebd8ff1c97f5953eaedca5ba49b9f3f8cff
ricardo <ricardoliveira50@hotmail.com>
2021-07-21 20:49:19 -0300

    Pagina home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CategoriaComponent } from './categoria/categoria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    RodapeComponent,
+    CategoriaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
